83e08daa6ef076d100e6b4e06b9c51a3420d9cad
bruce414 <brucezhang414@gmail.com>
2026-01-05 14:47:18 +1300

    feat: add money value object

---
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/valueobjects/Money.java
@@ -0,0 +1,28 @@
+package com.finflow.finflowbackend.ValueObjects;
+
+import com.finflow.finflowbackend.currency.Currency;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Embeddable
+public class Money {
+
+    @Column(nullable = false, precision = 19, scale = 6)
+    private BigDecimal amount;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "code")
+    private Currency currency;
+
+    protected Money(){}
+
+    public Money(BigDecimal amount, Currency currency) {
+        if (amount == null ||  currency == null) {
+            throw new IllegalArgumentException("Money amount and currency cannot be null");
+        }
+
+        this.amount = amount;
+        this.currency = currency;
+    }
+}
