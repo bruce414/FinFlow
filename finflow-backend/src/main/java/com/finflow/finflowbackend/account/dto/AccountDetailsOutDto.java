8eebe39923a659637efbc5c453d0965f2ecaa203
bruce414 <brucezhang414@gmail.com>
2026-01-24 14:49:42 +1300

    feat: add AccountDetailsOutDto

--- finflow-backend/src/main/java/com/finflow/finflowbackend/account/dto/AccountDetailsOutDto.java
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/account/dto/AccountDetailsOutDto.java
@@ -1,4 +1,22 @@
 package com.finflow.finflowbackend.account.dto;
 
-public record AccountDetailsOutDto() {
-}
+import com.finflow.finflowbackend.common.enums.AccountOrigin;
+import com.finflow.finflowbackend.common.enums.AccountType;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public record AccountDetailsOutDto(
+    UUID accountId,
+    AccountType accountType,
+    AccountOrigin accountOrigin,
+    String providerAccountName,
+    String accountDisplayName,
+    String accountNumberLast4,
+    String institutionName,
+    String institutionCode,
+    BigDecimal accountBalance,
+    String accountCurrencyCode,
+    String accountCurrencyName,
+    boolean active
+) {}
