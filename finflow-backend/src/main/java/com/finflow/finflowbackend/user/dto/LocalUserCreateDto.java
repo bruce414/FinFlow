10b445d8576ddf9574b39f3aa969f93256cfc541
bruce414 <brucezhang414@gmail.com>
2026-01-26 23:49:24 +1300

    feat: add LocalUserCreateDto

---
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/user/dto/LocalUserCreateDto.java
@@ -0,0 +1,37 @@
+package com.finflow.finflowbackend.user.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public record LocalUserCreateDto(
+        @NotBlank
+        @Size(max = 100)
+        String firstName,
+
+        @NotBlank
+        @Size(max = 100)
+        String lastName,
+
+        @NotBlank
+        @Email
+        @Size(max = 100)
+        String email,
+
+        @Size(max = 16)
+        @Pattern(regexp = "^\\+[1-9]\\d{7,14}$")
+        String phoneNumber,
+
+        @NotBlank
+        @Size(min = 8, max = 72)
+        String Password,
+
+        @NotNull
+        @Past
+        LocalDate dateOfBirth,
+
+        @NotBlank
+        @Size(max = 100)
+        String timeZone
+) {
+}
