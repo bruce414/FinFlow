ce788a167e119c64edab15bcda32c009a7b90549
bruce414 <brucezhang414@gmail.com>
2026-01-26 15:08:34 +1300

    feat: add TransactionCreateDto as well as refactored the money fields in AccountCreateDto

--- finflow-backend/src/main/java/com/finflow/finflowbackend/transaction/dto/TransactionCreateDto.java
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/transaction/dto/TransactionCreateDto.java
@@ -1,5 +1,27 @@
 package com.finflow.finflowbackend.transaction.dto;
 
-public record TransactionCreateDto(
+import com.finflow.finflowbackend.common.dtos.money.MoneyRequestDto;
+import com.finflow.finflowbackend.common.enums.CounterpartyType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 
+import java.time.LocalDate;
+
+public record TransactionCreateDto(
+    @NotNull
+    @Valid
+    MoneyRequestDto moneyRequest,
+
+    @NotNull
+    LocalDate postedDate,
+
+    @Size(max = 255)
+    String reference,
+
+    @NotBlank
+    @Size(max = 255)
+    String counterpartyName,
+
+    @NotNull
+    CounterpartyType counterpartyType
 ) {}
