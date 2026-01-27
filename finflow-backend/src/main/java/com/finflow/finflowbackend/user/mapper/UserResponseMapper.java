46a6f05f5d49021b9d53b8fd7349c9485903c6ac
bruce414 <brucezhang414@gmail.com>
2026-01-27 14:14:38 +1300

    feat: remove the user create and patch mapping method and renamed the file to only contain response related method(s)

--- finflow-backend/src/main/java/com/finflow/finflowbackend/user/mapper/UserResponseMapper.java
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/user/mapper/UserResponseMapper.java
@@ -3,23 +3,12 @@ package com.finflow.finflowbackend.user.mapper;
 import com.finflow.finflowbackend.user.User;
 import com.finflow.finflowbackend.user.dto.LocalUserCreateDto;
 import com.finflow.finflowbackend.user.dto.UserDetailsOutDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.finflow.finflowbackend.user.dto.UserPatchDto;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public interface UserResponseMapper {
 
     //Entity -> DTO
     UserDetailsOutDto userToUserDetailsOutDto(User user);
-
-    //DTO -> Entity
-    @Mapping(target = "userId", ignore = true)
-    @Mapping(target = "authMethod", ignore = true)
-    @Mapping(target = "emailVerified", ignore = true)
-    @Mapping(target = "passwordHash", ignore = true)
-    @Mapping(target = "status", ignore = true)
-    @Mapping(target = "lastLoginAt", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    User toLocalUserEntity(LocalUserCreateDto localUserCreateDto);
 }
