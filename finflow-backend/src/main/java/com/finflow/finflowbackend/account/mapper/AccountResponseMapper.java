c69bb2f56b884f85bbf837ddc44e2db3f1ef2234
bruce414 <brucezhang414@gmail.com>
2026-01-30 00:17:23 +1300

    wip: check point, needs to create a MoneyMapper first to maintain easy mappings between money properties

--- finflow-backend/src/main/java/com/finflow/finflowbackend/account/mapper/AccountResponseMapper.java
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/account/mapper/AccountResponseMapper.java
@@ -2,9 +2,15 @@ package com.finflow.finflowbackend.account.mapper;
 
 import com.finflow.finflowbackend.account.Account;
 import com.finflow.finflowbackend.account.dto.AccountDetailsOutDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public interface AccountResponseMapper {
 
     //Account -> AccountDetailsOutDto
+    @Mapping(source = "id", target = "accountId")
+    @Mapping(source = "accountMoney.amount", target = "money.amount")
+    @Mapping(source = "accountMoney.currency", target = "money.currencyCode")
     AccountDetailsOutDto toAccountDetailsOutDto(Account account);
 }
