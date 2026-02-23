730d2f8d673e95bb3049334f1170fda9cea23353
bruce414 <brucezhang414@gmail.com>
2026-02-23 13:56:05 +0800

    refactor: add transactionId so that the endpoint can return the full details about the transaction

--- finflow-backend/src/main/java/com/finflow/finflowbackend/transaction/dto/TransactionDetailsOutDto.java
+++ finflow-backend/src/main/java/com/finflow/finflowbackend/transaction/dto/TransactionDetailsOutDto.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 public record TransactionDetailsOutDto(
     UUID accountId,
+    UUID transactionId,
     TransactionOrigin origin,
     TransactionDirection direction,
     TransactionType type,
